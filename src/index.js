fd73c77f34724ad8b120b3ff1b1741996c66bb8d
Арина <arina.senchuk@gmail.com>
2019-03-24 16:29:52 +0300

    did the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let maxSM = 0;
+    let degree = 0;
+
+    searchSM(base);
+
+    function searchSM(baseNum) {
+        let arr = [];
+        for(let i = 2; i <= base; i++){
+            if(baseNum === i) {
+                arr.push(i);
+                break;
+            }
+            if (baseNum % i === 0) {
+                arr.push(i);
+                baseNum = baseNum /i;
+                i--;
+            }
+        }
+
+        maxSM = Math.max.apply(null, arr);
+
+        for(let i = 0; i < arr.length; i++) {
+            if (arr[i] === maxSM){
+                degree++
+            }
+        }
+    }
+
+
+    let zeroes = 0;
+    while(number > 0) {
+        number = Math.floor(number/maxSM);
+        zeroes += number;
+    }
+    return Math.floor(zeroes/degree);
+
+};
